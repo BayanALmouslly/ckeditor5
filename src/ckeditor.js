2b8884679495a529649b857ec324bb11a26f1bf6
bayan almouslly <bayan.almouslly@hotmail.com>
2023-06-20 05:29:15 +0200

    add ckeditor5-html-support

---
+++ src/ckeditor.js
@@ -0,0 +1,237 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
+
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption';
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Font from '@ckeditor/ckeditor5-font/src/font';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak';
+// import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
+// import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
+// import { Style } from '@ckeditor/ckeditor5-style';
+// import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+
+
+class Editor extends ClassicEditor { }
+
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Autoformat,
+	BlockQuote,
+	Bold,
+	Code,
+	Subscript,
+	Superscript,
+	Underline,
+	Strikethrough,
+	Essentials,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	ImageResize,
+	LinkImage,
+	Base64UploadAdapter,
+	Indent,
+	Italic,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	TableProperties,
+	TableCellProperties,
+	TableCaption,
+	Clipboard,
+	Alignment,
+	Font,
+	HorizontalLine,
+	PageBreak,
+	TodoList,
+	Highlight,
+	// SourceEditing,
+	// GeneralHtmlSupport,
+	// Style,
+	// RemoveFormat
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'underline', 'strikethrough',
+			 'code', 'subscript', 'superscript',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'todoList',
+			'alignment:left', 'alignment:right', 'alignment:center', 'alignment:justify',
+			'horizontalLine',
+			'|',
+			'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor',
+			 'highlight',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'imageUpload',
+			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
+			'undo',
+			'redo',
+			'pageBreak',
+			// 'style',
+			// 'removeFormat',
+		]
+	},
+	language: 'en',
+	image: {
+		toolbar: [
+			'imageStyle:block',
+			'imageStyle:side',
+			'imageStyle:inline',
+			'|',
+			'toggleImageCaption',
+			'imageTextAlternative',
+			'|',
+			'linkImage'
+		],
+		resizeUnit: 'px',
+		resizeOptions: [
+			{
+				name: 'resizeImage:original',
+				label: 'Original',
+				value: null
+			},
+			{
+				name: 'resizeImage:50',
+				label: '50px',
+				value: '50'
+			},
+			{
+				name: 'resizeImage:75',
+				label: '75px',
+				value: '75'
+			},
+			{
+				name: 'resizeImage:100',
+				label: '100px',
+				value: '100'
+			},
+			{
+				name: 'resizeImage:150',
+				label: '150px',
+				value: '150'
+			},
+			{
+				name: 'resizeImage:200',
+				label: '200px',
+				value: '200'
+			},
+			{
+				name: 'resizeImage:250',
+				label: '250px',
+				value: '250'
+			},
+			{
+				name: 'resizeImage:300',
+				label: '300px',
+				value: '300'
+			}
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn', 'tableRow', 'mergeTableCells',
+			'tableProperties', 'tableCellProperties',
+			'toggleTableCaption'
+		],
+	},
+	highlight: {
+		options: [
+			{ model: 'yellowMarker', class: 'marker-yellow', title: 'Yellow Marker', color: 'var(--ck-highlight-marker-yellow)', type: 'marker' },
+			{ model: 'greenMarker', class: 'marker-green', title: 'Green marker', color: 'var(--ck-highlight-marker-green)', type: 'marker' },
+			{ model: 'pinkMarker', class: 'marker-pink', title: 'Pink marker', color: 'var(--ck-highlight-marker-pink)', type: 'marker' },
+			{ model: 'blueMarker', class: 'marker-blue', title: 'Blue marker', color: 'var(--ck-highlight-marker-blue)', type: 'marker' },
+			{ model: 'redPen', class: 'pen-red', title: 'Red pen', color: 'var(--ck-highlight-pen-red)', type: 'pen' },
+			{ model: 'greenPen', class: 'pen-green', title: 'Green pen', color: 'var(--ck-highlight-pen-green)', type: 'pen' }
+		]
+	},
+	indentBlock: {
+		offset: 1,
+		unit: 'em'
+	},
+	// htmlSupport: {
+	// 	allow: [
+	// 		{
+	// 			name: /.*/,
+	// 			attributes: true,
+	// 			classes: true,
+	// 			styles: true
+	// 		}
+	// 	]
+	// },
+	// style: {
+	// 	definitions: [
+	// 		{
+	// 			name: 'Article category',
+	// 			element: 'h3',
+	// 			classes: [ 'category' ]
+	// 		},
+	// 		{
+	// 			name: 'Info box',
+	// 			element: 'p',
+	// 			classes: [ 'info-box' ]
+	// 		},
+	// 	]
+	// }
+};
+
+export default Editor;
